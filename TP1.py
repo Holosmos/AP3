e34a462f2c5d9d7a2adf8af639539bfb448471ce
Holosmos <raphaelalexandre.2509@gmail.com>
2014-09-25 16:09:01 +0200

    cor

--- TP1.py
+++ TP1.py
@@ -16,9 +16,9 @@ def milieu(n,m,p):
 	if a == p:
 		return maximum(n,m)
 	return milieu(p,n,m)	
-print(milieu(1,2,3))
-print(milieu(1,3,2))
-print(milieu(3,1,3))
+#print(milieu(1,2,3))
+#print(milieu(1,3,2))
+#print(milieu(3,1,3))
 
 def croucrou(n,m,p):
 	a = max(n,m)
@@ -35,9 +35,9 @@ def croucrou(n,m,p):
 		else:
 			return max(n,m)
 						
-print(croucrou(1,2,3))
-print(croucrou(1,3,2))
-print(croucrou(3,1,3))		
+#print(croucrou(1,2,3))
+#print(croucrou(1,3,2))
+#print(croucrou(3,1,3))		
 
 i = 1
 a= "0"
@@ -48,8 +48,7 @@ while i<= 10:
 
 l = [1,11,3,45,67,2,456]
 
-for i in range(len(l)):
-	a = l[i]
+for a in l:
 	#if a <= 10:
 		#print(a)
 		
@@ -66,7 +65,7 @@ def somme_liste_entier(l,n,m):
 	if n>=a or m>= a or n<0 or m<0:
 		return "Erreur de portee"
 	somme=0
-	for i in range(a):
+	for i in range(n,m):
 		case = l[i]
 		if type(case) is int:
 			somme+= l[i]
@@ -74,10 +73,10 @@ def somme_liste_entier(l,n,m):
 			return "Erreur de types"		
 	return somme
 	
-#print somme_liste_entier([1,2,3],0,2)
-#print somme_liste_entier([1,2,3],0,3)
-#print somme_liste_entier([1,2,3],2,2)
-#print somme_liste_entier([1,2,'a'],0,2)	
+print somme_liste_entier([1,2,3],0,2)
+print somme_liste_entier([1,2,3],0,3)
+print somme_liste_entier([1,2,3],2,2)
+print somme_liste_entier([1,2,'a'],0,2)	
 
 
 def sous_chaine(ch,n,m):
@@ -93,14 +92,13 @@ def sous_chaine(ch,n,m):
 #print(sous_chaine("Coucou tout le monde !",10,10))		
 
 def PairsImpairs(x):
-	chaine = "Nombres pairs :"
+	chaine1 = "Nombres pairs :"
+	chaine2 = "Nombres impairs :"
 	for i in range(1,x):
-		chaine += " " + str(2*i)
-	print(chaine)
-	chaine = "Nombres impairs :"
-	for i in range(1,x):
-		chaine += " " + str(2*(x-i)-1)
-	print(chaine)		
+		chaine1 += " " + str(2*i)
+		chaine2 += " " + str(2*(x-i)-1)
+	print(chaine1)
+	print(chaine2)		
 	
 
 #parser = argparse.ArgumentParser()
